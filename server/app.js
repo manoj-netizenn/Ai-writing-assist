87ff783c4259bda562141b1236a95cb7a3806634
manoj-netizenn <mallappanavarmanoj@gmail.com>
2025-06-26 21:27:43 +0530

    first commit

--- server/app.js
+++ server/app.js
@@ -8,7 +8,13 @@ const spellChecker = require("./routes/spellChecker");
 const app = express();
 const port = 5000 || process.env.PORT;
 
-app.use(cors());
+app.use(cors
+  ({
+  origin: 'https://brilliant-bunny-d140c2.netlify.app', 
+  methods: ['GET', 'POST'],
+  credentials: true
+})
+);
 app.use(express.json());
 
 app.use("/api/analyze", analyzeRouter);
